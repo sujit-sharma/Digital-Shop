89332e1b9e164b3c79a4f3d15183ce2a6bf8f12c
Sujit Sharma <sujit.dhakal1166@gmail.com>
2019-04-29 14:28:43 +0545

    Dynamic content

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,23 @@
-const express = require('express');
 const path = require('path');
-const rootDir = require('../util/path');
 
+const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.use('/add-product',(req,res,next) => {
-    res.sendFile(path.join(rootDir,'routes','views','add-product.html'));
-    //res.send('<form action ="/product" method = "POST"> <input type = "text" name = "title" <button type ="submit">Add Product</button></form>');
+const products = [];
 
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
 });
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-    //res.send('<h3> Thank you </h3>');
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
-module.exports = router;
- 
+
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,22 @@
+const path = require('path');
+
 const express = require('express');
 
-const path = require('path');
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/shop',(req,res,next) => {
-    res.sendFile(path.join(__dirname,'/views','shop.html'));
-    //res.sendFile('/views/shop.html');
-    //
-
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
- 
